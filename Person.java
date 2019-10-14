bf8e97d14d799b659fae3458f0480f86b578e663
Cheryl <cheryl.m.villator-1@ou.edu>
2019-10-14 13:44:10 -0500

    Person and PersonTest

---
+++ Person.java
@@ -0,0 +1,24 @@
+
+public class Person extends NameHolder {
+	private String lastName;
+	
+	public Person() throws NameException {
+		String temp1 = "Bo";
+		String temp2 = "Smith";
+		lastName = temp1;
+		super.setFirstName(temp2);
+	}
+	
+	public Person(String firstName, String lastName) throws NameException {
+		this.lastName = firstName;
+		
+		super.setFirstName(lastName);
+	}
+
+	public String describeSelf() {
+		String description = "*** First Name: " + super.getFirstName()
+				+ " Last Name: " + lastName + "***";
+		return description;
+		//return "I am so failing"; //not in life!
+	}
+}
