91f24e7869cf43d7183feb56b79b539e6c6724c4
Cheryl <cheryl.m.villator-1@ou.edu>
2019-10-16 14:12:30 -0500

    Add Student compareTo method

--- StudentTest.java
+++ StudentTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -12,22 +13,59 @@ class StudentTest {
 	}
 
 	@Test
-	public void testStudent() {
-		Student s;
+	public void testStudent() { 
 		try {
-			s = new Student();
+		Student	s = new Student();
 		
 		Integer rating = s.getRating();
 		Integer expected = INITIAL_RATING;
 		
 		assertEquals(expected, rating);
 		} catch (NameException e) {
-			fail("");
+			fail();
 		}
 	}
+	@Test
+	public void testCompareTo() throws NameException{
+		Student firstStudent = new Student("x", "Y", 2);
+		Student secondStudent = new Student("x", "Y", 3);		
+		
+		boolean result = false;
+		int actual = secondStudent.compareTo(firstStudent);
+		// should be bigger than zero
+		if (actual > 0) {
+			result = true;
+		}
+		assertTrue(result);
+
+		result = false;
+		Student student = new Student("x", "Y", 5);	
+		
+		actual = secondStudent.compareTo(student);
+		if(actual < 0) {
+			result = true;
+		}
+		assertTrue(result);
+		
+		result = false;
+		
+		Student studentSame =  new Student("x", "Y", 3);
+		actual = secondStudent.compareTo(studentSame);
+		int expected = 0;
+		assertEquals(expected, actual);
+		
+	}
 	
-	public void testCompareTo() {
-		fail("Not yet implemented");
+	@Test
+	public void testGet() throws NameException {
+		Integer expected = 4;
+		Integer rating = expected;
+		String firstName = "Alex";
+		String lastName = "Bo";
+		Student s = new Student(firstName, lastName, rating);
+		Integer actual = s.getRating();
+		assertEquals(expected, actual);
+		
 	}
 
 }
